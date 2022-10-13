b7aeb38f44461c42b8b2ecd0477bcdd2b4a7f798
Samuel Espejo Gil <sami.espj@gmail.com>
2022-10-13 12:18:38 +0200

    Eliminadas las pruebas de muestra de Git y GitHub
    El main está exactamente igual que en el whatsapp, a partir de aquí
    trabajaremos.

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,45 @@
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Main {
 
-	public static void main(String[] args) {	
-		System.out.println("A samuel le gustan los penes");
-		System.out.println("mentira");
-		// lo que sea
-		// otra cosa
-		// Hola crack
-		
-		// Comentario desde la página web
+	public static void main(String[] args) {
 
+		Stack<String> s = new Stack<String>();
+
+		Scanner leerfichero = null;
+		boolean seguir = true;
+		try {
+			File fichero = new File("navegacion.txt"); // este fichero debe estar al mismo nivel que la carpeta src del proyecto, no dentro de esta
+			leerfichero = new Scanner(fichero);
+		} catch (FileNotFoundException e) {
+			System.out.println("Fichero no encontrado");
+			seguir = false;
+		}
+
+		int tiempototal = 0, cuentapaginas= 0;
+		if (seguir) {
+			while (leerfichero.hasNextLine()) {
+				String linea = leerfichero.nextLine();
+				String[] partes; // guardará la URL en [0] y el tiempo en [1]
+				if (linea.equals("<=")) { // si es una vuelta atrás
+					s.pop(); // Se quita la página en la que estábamos
+					partes = s.peek().split(","); // se separan la página y el tiempo de la anterior
+				} else { // es una URL
+					s.push(linea); // se introduce a la pila
+					partes = linea.split(","); // esa URL se separa del tiempo
+				}
+
+				cuentapaginas++; // se ha visitado una página más
+				tiempototal += Integer.parseInt(partes[1]); // se suma el tiempo de esa página visitada
+				System.out.println(partes[0]); // se muestra la página visitada
+			}
+
+		}
+		System.out.println("El tiempo medio en las paginas es de: "+ tiempototal/cuentapaginas);
 	}
 
-	static void hola() {
-		System.out.println("hola");
-	}
 }
