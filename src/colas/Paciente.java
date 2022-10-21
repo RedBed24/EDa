f3d693a6796f7d7ca546a56a1c37d58a1f5275a5
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2022-10-21 10:14:37 +0200

    Actualización de la clase Paciente
    Implementación del método compareTo(), devuelve el valor necesario para
    que se puedan añadir correctamente a la cola con prioridad atendiendo a
    la prioridad que tienen

--- src/colas/Paciente.java
+++ src/colas/Paciente.java
@@ -1,8 +1,35 @@
 package colas;
 
 public class Paciente implements Comparable<Paciente> {
-
-	public int compareTo(Paciente o) {
-		  return 0;
+	
+	private int severity;
+	private int speciality;
+	
+	public Paciente(int severity, int speciality) {
+		this.severity= severity;
+		this.speciality= speciality;
 	}
+
+	public int compareTo(Paciente other) {
+		if (severity > other.severity) return 1;
+		if (severity < other.severity) return -1;
+		return 0;
+	}
+	
+	public int getSeverity() {
+		return severity;
+	}
+
+	public void setSeverity(int severity) {
+		this.severity = severity;
+	}
+
+	public int getSpeciality() {
+		return speciality;
+	}
+
+	public void setSpeciality(int speciality) {
+		this.speciality = speciality;
+	}
+	
 }
