c0fa560e4bddb6acc063264c36094af5db94449e
Salamander <Salamander@Dominem>
2022-10-24 22:19:30 +0200

    Severity y Speciality y fichero de ejemplo (a modificar)

--- src/colas/Severity.java
+++ src/colas/Severity.java
@@ -1,5 +1,29 @@
 package colas;
 
 public enum Severity {
+	VITAL("Vital",2), SEVERE("Severe",1), MILD("Mild",0);
+	
+	private String gravedad;
+	private int prioridad;
+	
+	private Severity(String gravedad, int prioridad) {
+		this.gravedad = gravedad;
+		this.prioridad = prioridad;
+	}
 
+	public String getGravedad() {
+		return gravedad;
+	}
+
+	public void setGravedad(String gravedad) {
+		this.gravedad = gravedad;
+	}
+
+	public int getPrioridad() {
+		return prioridad;
+	}
+
+	public void setPrioridad(int prioridad) {
+		this.prioridad = prioridad;
+	}
 }
--- src/colas/Speciality.java
+++ src/colas/Speciality.java
@@ -1,5 +1,29 @@
 package colas;
 
 public enum Speciality {
+	TRAUMATOLOGÍA("Traumatology", 0), CARDIOLOGÍA("Cardiology", 1), NEUROLOGÍA("Neurology", 2);
+	
+	private String especialidad;
+	private int departamento;
+	
+	private Speciality(String especialidad, int departamento) {
+		this.especialidad = especialidad;
+		this.departamento = departamento;
+	}
 
+	public String getEspecialidad() {
+		return especialidad;
+	}
+
+	public void setEspecialidad(String especialidad) {
+		this.especialidad = especialidad;
+	}
+
+	public int getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(int departamento) {
+		this.departamento = departamento;
+	}
 }
