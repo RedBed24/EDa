4ee9f4affa591304b3aed716b4afcc670bdbb58f
javicuartero <86930738+javicuartero@users.noreply.github.com>
2022-12-25 21:02:54 +0100

    Peticiones al usuario en Principal.menú ID, reservas

--- src/B2_03_trabajo/Principal.java
+++ src/B2_03_trabajo/Principal.java
@@ -6,25 +6,25 @@ import java.util.Scanner;
 public class Principal {
 	
 	final static Scanner TECLADO = new Scanner(System.in);
-	final static String MENÃš = "\nIntroduce una opciÃ³n:\n 1: Ver el tren\n 2: Consultar ocupante de un asiento\n "
-			+ "3: numPasajeros\n 4: trenLleno\n 5: reservarAsiento\n 6: liberarAsiento\n 7: Salir del programa";
+	final static String MENÚ = "\nIntroduce una opción:\n 1: Ver el tren\n 2: Consultar ocupante de un asiento\n "
+			+ "3: numPasajeros\n 4: trenLleno\n 5: reservarAsiento\n 6: liberarAsiento\n 7: Reservar varios asientos\n 0: Salir del programa";
 	
 	public static void main(String[] args) {
 		// Bienvenida
-					System.out.println("\n*****************************************\n* ESTRUCTURA DE DATOS - TRABAJO TEÃ“RICO *\n*****************************************");
+					System.out.println("\n*****************************************\n* ESTRUCTURA DE DATOS - TRABAJO TEÓRICO *\n*****************************************");
 					
 		Tren tren = new Tren(1, 6);
-		menÃº(tren);
+		menú(tren);
 		System.out.println(tren);
 	}
 
-	public static void menÃº(Tren tren) {
+	public static void menú(Tren tren) {
 		String identificador;
 		while (true) { 
 			try {
-				// Muestra del menÃº
-				System.out.println(MENÃš);
-				// ComprobaciÃ³n de la opciÃ³n elegida por el usuario
+				// Muestra del menú
+				System.out.println(MENÚ);
+				// Comprobación de la opción elegida por el usuario
 				switch (TECLADO.nextInt()) {
 				
 					// Ver el tren
@@ -43,7 +43,7 @@ public class Principal {
 					
 					// trenLleno
 					case 4:
-						System.out.println(tren.trenLleno());
+						System.out.println(tren.isLleno());
 						break;
 						
 					// reservarAsiento
@@ -55,38 +55,38 @@ public class Principal {
 					// liberarAsiento
 					case 6:
 						System.out.println("Introduzca el identificador del asiento a liberar.");
-						System.out.println(tren.liberarAsiento(TECLADO.next());
+						System.out.println(tren.liberarAsiento(TECLADO.next()));
 						break;
 					
-					// TODO: AÃ±adir vagÃ³nÂ¿? Se aÃ±aden automÃ¡ticamente al llenar uno
+					// TODO: Añadir vagón¿? Se añaden automáticamente al llenar uno
 					// Nos piden reservar x asientos
 					case 7:
-						System.out.println("Introduzca un identificador para la compra de varios asientos, se le aÃ±adirÃ¡ automÃ¡ticamente un nÃºmero para diferenciarlos.");
+						System.out.println("Introduzca un identificador para la compra de varios asientos, se le añadirá automáticamente un número para diferenciarlos.");
 						identificador = TECLADO.next();
 						System.out.println("Introduzca la cantidad de reservas que quiera realizar.");
 						final int reservas = TECLADO.nextInt();
 						for (int i = 0; i < reservas; i++)
-							// igual es muy redundante el sysout y deberÃ­amos controlar el caso de null
+							// igual es muy redundante el sysout y deberíamos controlar el caso de null
 							System.out.println(tren.reservarAsiento(identificador+i));
 						System.out.println("Se han realizado " + reservas + " reservas.");
 						break;
 						
-					// OpciÃ³n de salida	
+					// Opción de salida	
 					case 0:
 						TECLADO.close(); return;
 					
-					// Se ha introducido una opciÃ³n fuera de las contempladas
-					default: System.err.println("OpciÃ³n seleccionada invÃ¡lida");
+					// Se ha introducido una opción fuera de las contempladas
+					default: System.err.println("Opción seleccionada inválida");
 				}
 				
 			} catch (InputMismatchException e) {
-				System.err.println("Error al introducir la selecciÃ³n del menÃº.");
-				TECLADO.next(); // Se quita el valor errÃ³neo del bÃºffer
+				System.err.println("Error al introducir la selección del menú.");
+				TECLADO.next(); // Se quita el valor erróneo del búffer
 				
 			} catch (IllegalArgumentException e) {
 				System.err.println(e.getMessage());
 			} catch (Exception e) {
-				System.err.println("Error inesperado, se finalizarÃ¡ el programa.");
+				System.err.println("Error inesperado, se finalizará el programa.");
 				return ;
 			}
 
